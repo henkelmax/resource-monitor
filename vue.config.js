91eee66e3b30a602091b750fc109ff363fbdd9db
Max Henkel <max@maxhenkel.de>
2020-12-23 17:02:15 +0100

    Add icons for linux and macos

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,45 @@
 module.exports = {
     productionSourceMap: false,
-}
+    pluginOptions: {
+      electronBuilder: {
+        builderOptions: {
+          extraFiles: [
+            "build/icon.ico"
+          ],
+          win: {
+            target: ["portable", "msi", "zip"],
+            icon: "build/icon.ico",
+            publish: [
+              {
+                "provider": "github",
+                "vPrefixedTagName ": false,
+                "releaseType": "release"
+              }
+            ]
+          },
+          linux: {
+            target: ["deb", "rpm", "apk", "zip"],
+            icon: "build/128x128.png",
+            publish: [
+              {
+                "provider": "github",
+                "vPrefixedTagName ": false,
+                "releaseType": "release"
+              }
+            ]
+          },
+          mac: {
+            target: ["dmg", "zip"],
+            icon: "build/icon.icns",
+            publish: [
+              {
+                "provider": "github",
+                "vPrefixedTagName ": false,
+                "releaseType": "release"
+              }
+            ]
+          }
+        }
+      }
+    }
+  }
